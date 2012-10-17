e4db51a7c9d4ffc80bd398f100218ed00386dcb9
Jakob Løkke Madsen <jakob@jakobloekkemadsen.com>
2012-10-17 16:08:45 +0200

    655649-37015531UI Made a very nice UX

--- app.js
+++ app.js
@@ -38,24 +38,34 @@ app.get('/api/items/:keyword', function(req,res){
     nodeio.scrape(function() {
         this.getHtml(url, function(err, $) {
 
-            var data = {
-                    name: keyword,
-                    prices: [],
-                    listings: $('.listing')
-                };
+            if (err) {
+                console.log(err);
+            } else {
+                var data = {
+                        name: keyword,
+                        prices: []
+                    };
 
-            // Extract all prices from search results
-            $('td[title=Pris]').each(function(listing) {
-                var raw_price = listing.text,
-                    cleaned_price = parseFloat(raw_price.replace("Kr. ", "").replace(".", ""));
+                // Extract all prices from search results
+                $('td[title=Pris]').each(function(listing) {
+                    var raw_price = listing.text,
+                        cleaned_price = parseFloat(raw_price.replace("Kr. ", "").replace(".", ""));
 
-                data.prices.push(cleaned_price);
-            });
+                    data.prices.push(cleaned_price);
+                });
 
-            data.median_price = median(data.prices);
-            data.average_price = average(data.prices);
+                if (data.prices.length > 0) {
+                    data.median_price = median(data.prices);
+                    data.average_price = average(data.prices);
+                    data.lowest_price = lowest(data.prices);
+                    data.highest_price = highest(data.prices);
+                    data.quality = quality(data.prices);
+                }
 
-            res.json(data);
+                res.json(data);
+
+            }
+
         });
     });
 });
@@ -80,10 +90,22 @@ function average(values) {
     return Math.floor(sum/values.length);
 }
 
+function highest(values) {
+    return Math.max.apply( Math, values );
+}
 
+function lowest(values) {
+    return Math.min.apply( Math, values );
+}
 
+function quality(values) {
+    var diff_from_high = highest(values) - median(values),
+        diff_from_low = median(values) - lowest(values),
+        diff_diff = diff_from_high - diff_from_low,
+        quality = Math.log(diff_diff / values.length, 2);
 
-
+    return quality;
+}
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- public/javascripts/powertools.js
+++ public/javascripts/powertools.js
@@ -16,8 +16,8 @@ powertools.controller('ItemsCtrl', function ($scope, $http, Item) {
     $scope.total = function() {
         var total = 0;
         $scope.list.forEach(function(item){
-            if (typeof item.medianprice !== 'undefined') {
-                total = total + item.medianprice;
+            if (typeof item.median_price !== 'undefined') {
+                total = total + item.median_price;
             }
         });
         return total;
@@ -26,57 +26,11 @@ powertools.controller('ItemsCtrl', function ($scope, $http, Item) {
     $scope.loading = function() {
         var list_length = $scope.list.length,
             last_item = (typeof $scope.list[list_length - 1] !== 'undefined') ? $scope.list[list_length - 1] : '';
-
         return $scope.list.length > 0 && !!(typeof last_item.name === 'undefined');
     };
 
     $scope.removeItem = function() {
         $scope.list.splice(this.$index,1);
-    }
-});
-
-
-
-
-
-powertools.controller('Old_ItemsCtrl', function ($scope, $http) {
-    $scope.list = [];
-
-    $scope.total = function() {
-        var total = 0;
-
-        $scope.list.forEach(function(item){
-            total = total + item.medianprice;
-        });
-
-        return total;
     };
 
-    $scope.addItem = function() {
-        $http({method: 'GET', url: '/api/items/' + $scope.itemName}).
-            success(function(data) {
-                $scope.data = data;
-
-                //Update list:
-                $scope.list.push(
-                    {
-                        name: $scope.itemName,
-                        count: $scope.data.prices.length,
-                        medianprice: $scope.data.median_price,
-                        averageprice: $scope.data.average_price
-                    }
-                );
-
-                $scope.itemName = '';
-
-            }).
-            error(function(data, status) {
-                $scope.data = data || "Request failed";
-                $scope.status = status;
-            });
-    };
-
-    $scope.removeItem = function() {
-        $scope.list.splice(this.$index,1);
-    }
 });
